d62639327aecb7807eba93943ffac21461123ac3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-02-24 13:24:52 +0100

    Added Android LineBreakMode test

--- src/Platform.Handlers/tests/Xamarin.Platform.Handlers.DeviceTests/Handlers/Label/LabelHandlerTests.Android.cs
+++ src/Platform.Handlers/tests/Xamarin.Platform.Handlers.DeviceTests/Handlers/Label/LabelHandlerTests.Android.cs
@@ -1,6 +1,9 @@
 ﻿using System.Threading.Tasks;
+using Android.Text;
 using Android.Widget;
 using Xamarin.Forms;
+using Xamarin.Platform.Handlers.DeviceTests.Stubs;
+using Xunit;
 
 namespace Xamarin.Platform.Handlers.DeviceTests
 {
@@ -25,5 +28,33 @@ namespace Xamarin.Platform.Handlers.DeviceTests
 
 		int GetNativeMaxLines(LabelHandler labelHandler) =>
 			GetNativeLabel(labelHandler).MaxLines;
+
+		TextUtils.TruncateAt GetNativeEllipsize(LabelHandler labelHandler) =>
+			GetNativeLabel(labelHandler).Ellipsize;
+
+		[Fact(DisplayName = "[LabelHandler] LineBreakMode Initializes Correctly")]
+		public async Task LineBreakModeInitializesCorrectly()
+		{
+			var xplatLineBreakMode = LineBreakMode.TailTruncation;
+
+			var labelStub = new LabelStub()
+			{
+				LineBreakMode = xplatLineBreakMode
+			};
+
+			var expectedValue = TextUtils.TruncateAt.End;
+
+			var values = await GetValueAsync(labelStub, (handler) =>
+			{
+				return new
+				{
+					ViewValue = labelStub.LineBreakMode,
+					NativeViewValue = GetNativeEllipsize(handler)
+				};
+			});
+
+			Assert.Equal(xplatLineBreakMode, values.ViewValue);
+			Assert.Equal(expectedValue, values.NativeViewValue);
+		}
 	}
 }
